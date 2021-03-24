dbd7f71b96d4e9a18ac00dddc14937c35fba5b04
Case <roossienkees@gmail.com>
2021-03-24 20:33:41 +0100

    Stoplight integration

--- ESP32/main.py
+++ ESP32/main.py
@@ -1,46 +1,53 @@
 import time
 import random
+import config
+import machine
 from boot import wlan_connect, wlan_check, stoplight
-
-serverUrl = "http://example.com/post-esp-data.php"
-keyAPI = "rNum"
-
-sensorName = "ESP32/2"
-sensorLocation = "GPS data"
-
-device_on = True
 
 
 def temp_sensor():
     """Random reading returned as float.
     """
     time.sleep(2)
-    return random.uniform(32, 38)
+    return random.uniform(30, 40)
 
 
 def scan():
     """Scanning logic
     """
     scanning = True
-    trigger = 37.1
     while scanning:
         stoplight("yellow")
-        if temp_sensor() > trigger:
+        print("Scanning...")
+
+        reading = temp_sensor()
+        if reading > config.trigger:
+            print("Trigger value exceeded, red light")
             stoplight("red")
-        elif temp_sensor() < trigger:
+        elif reading < config.trigger:
+            print("Scan successful, green light")
             stoplight("green")
+
+        time.sleep(2)       # sleeping so output can be viewed.
+        scanning = False
 
 
 def loop():
     """Continues loop
     """
     count = 0
-    while device_on:
+    while True:
         count += 1
         if count % 10:
             if not wlan_check():
                 wlan_connect()
-        scan()
+        if config.device_on:
+            scan()
+        else:
+            stoplight("red")
+            # if machine.reset_cause() == machine.DEEPSLEEP_RESET:
+            #     print("Woke from a deep sleep...")
+            # machine.deepsleep(10000)
 
 
 if __name__ == "__main__":
