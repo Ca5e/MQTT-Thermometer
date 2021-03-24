7fb0bfc97cf1db83fbb72ac9f0058c0b62366992
Case <roossienkees@gmail.com>
2021-03-24 20:32:58 +0100

    Create config_example.py

---
+++ ESP32/config_example.py
@@ -0,0 +1,24 @@
+# config.py
+
+# Network configuration:
+SSID = "ESP-WiFi"
+WPA2 = "Johma Password"
+
+# Device information
+sensorName = "ESP32/2"
+sensorLocation = "GPS data"
+
+# Database connection
+serverUrl = "http://example.com/post-esp-data.php"
+keyAPI = "rNum87546392"
+
+
+# Specifics
+device_on = True
+trigger = 37.1
+
+# Stoplight GPIO pins
+redPin = 25
+ylwPin = 26
+grnPin = 27
+# Meaning red light is connected to '25', etc.
