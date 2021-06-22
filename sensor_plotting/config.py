425e86ecaa3444a7a4d03507ece3ae4f9f4ae78a
Case <roossienkees@gmail.com>
2021-06-22 21:44:41 +0200

    Create config.py

---
+++ sensor_plotting/config.py
@@ -0,0 +1,13 @@
+# mqtt
+broker = "localhost"
+port = 1883
+QOS = 1
+
+queue = {}
+reply_list = []
+
+standard_trigger = 37.1
+
+# processing
+topic_sub_stream = "sensor/+/reading/stream"
+topic_sub_online = "sensor/+/status/online"
