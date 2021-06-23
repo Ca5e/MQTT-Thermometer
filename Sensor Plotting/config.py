c1df09de516ec28152f91968ce1dfa3ae6817d01
Case <roossienkees@gmail.com>
2021-06-23 14:04:47 +0200

    Added configurables

--- Sensor Plotting/config.py
+++ Sensor Plotting/config.py
@@ -1,13 +1,24 @@
-# mqtt
+# MQTT
 broker = "localhost"
-port = 1883
+port = 443
 QOS = 1
 
+topic_sub_stream = "sensor/+/reading/stream"
+topic_publish_reply = "sensor/{}/reading/reply"
+
+# Processing
+standard_trigger = 37.1
+template = 'C:/Program Files/sensor plotting/head.png'
+scale = 20 # Amount the image should be scaled
+method = "cv2.TM_CCOEFF"
+
+display = False
+batch = 15
+publish = True
+
+# Alerts
+path = 'C:/Program Files/sensor plotting/alerts'
+
+# Initial
 queue = {}
 reply_list = []
-
-standard_trigger = 37.1
-
-# processing
-topic_sub_stream = "sensor/+/reading/stream"
-topic_sub_online = "sensor/+/status/online"
