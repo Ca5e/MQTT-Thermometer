77510ade064e879258514dbc77607fb61df012a3
Case <roossienkees@gmail.com>
2021-06-23 14:15:26 +0200

    Removed helper dependency

--- Sensor Plotting/main.py
+++ Sensor Plotting/main.py
@@ -3,9 +3,9 @@ Subscribes to the stream topic and starts processing thread.
 """
 import time
 import threading
+import datetime
 
 from mqtt import get_mqtt_client
-from helpers import get_now_string
 
 import processing
 import config
@@ -13,7 +13,7 @@ import config
 def on_message(client, userdata, msg):
     """Funtion that gets executed when a publish message is received.
     """
-    now = get_now_string()
+    now = datetime.datetime.now()
     sensor = msg.topic.split('/')[1]
     print(f"Stream data from {sensor} at {now}")
 
@@ -21,7 +21,7 @@ def on_message(client, userdata, msg):
     try:
         # if 'sensor's key has one or less items, start the thread:
         if len(config.queue[sensor]) <= 1:
-            x = threading.Thread(target=processing.start, args=(sensor, client, False))
+            x = threading.Thread(target=processing.start, args=(sensor, client))
             x.start()
     except Exception as e:
         print("on_message(): " + str(e))
