2b22f8bbb813e9d01eaf4759c532c00b1d4e37ac
Case <roossienkees@gmail.com>
2021-06-23 14:44:59 +0200

    Added randomness

--- Sensor Plotting/config.py
+++ Sensor Plotting/config.py
@@ -6,20 +6,22 @@ QOS = 1
 topic_sub_stream = "sensor/+/reading/stream"
 topic_publish_reply = "sensor/{}/reading/reply"
 
-publish = True    # Enable MQTT publishing after processing
+publish = True     # Enable MQTT publishing after processing
 
 # Processing
 standard_trigger = 37.1
 method = "cv2.TM_CCOEFF"
-template = 'C:/Program Files/sensor plotting/head.png'
-scale = 20        # Amount the image should be scaled
+template = "C:/Program Files/sensor plotting/head.png"
+scale = 20         # Amount the image should be scaled
 
-display = False   # Enable cv2.imshow()
-batch = 15        # Amount the sensor has to send before processing starts
+display = False    # Enable cv2.imshow()
+batch = 15         # Amount the sensor has to send before processing starts
+
+randomness = False # Replies are randomized to make simulation data more interesting.
 
 # Alerts
-path = 'C:/Program Files/sensor plotting/alerts'    # .jpg trigger image save path
+path = "C:/Program Files/sensor plotting/alerts"    # .jpg 'trigger image' save path
 
 # Initial
-queue = {}        # dictionary used to store sensor names and corresponding frames
-reply_list = []   # used to store results while testing
+queue = {}         # Dictionary used to store sensor names and corresponding frames
+reply_list = []    # Used to store results while testing
