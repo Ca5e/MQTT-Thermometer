173f3eb66bd27443045d9a051f2922e6ea00f658
Case <roossienkees@gmail.com>
2021-06-23 14:23:25 +0200

    Update config.py

--- Sensor Plotting/config.py
+++ Sensor Plotting/config.py
@@ -1,24 +1,25 @@
 # MQTT
 broker = "localhost"
-port = 443
+port = 1883
 QOS = 1
 
 topic_sub_stream = "sensor/+/reading/stream"
 topic_publish_reply = "sensor/{}/reading/reply"
 
+publish = True    # Enable MQTT publishing after processing
+
 # Processing
 standard_trigger = 37.1
-template = 'C:/Program Files/sensor plotting/head.png'
-scale = 20 # Amount the image should be scaled
 method = "cv2.TM_CCOEFF"
+template = 'C:/Program Files/sensor plotting/head.png'
+scale = 20        # Amount the image should be scaled
 
-display = False
-batch = 15
-publish = True
+display = False   # Enable cv2.imshow()
+batch = 15        # Amount the sensor has to send before processing starts
 
 # Alerts
-path = 'C:/Program Files/sensor plotting/alerts'
+path = 'C:/Program Files/sensor plotting/alerts'    # .jpg trigger image save path
 
 # Initial
-queue = {}
-reply_list = []
+queue = {}        # dictionary used to store sensor names and corresponding frames
+reply_list = []   # used to store results while testing
